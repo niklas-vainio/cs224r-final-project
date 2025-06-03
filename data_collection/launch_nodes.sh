b0840343eed78a0bb0b990e9918fbf9840df62ba
Niklas Vainio <niklasvainio8@gmail.com>
2025-06-03 12:00:23 -0700

    Minor cleanup

--- data_collection/launch_nodes.sh
+++ data_collection/launch_nodes.sh
@@ -1,3 +1,3 @@
 OG_GELLO_DIR="../external/og-gello"
 
-python "${OG_GELLO_DIR}/experiments/launch_nodes.py" --task_name picking_up_trash --recording_path $1
+python "${OG_GELLO_DIR}/experiments/launch_nodes.py" --robot R1Pro --task_name picking_up_trash --recording_path $1
